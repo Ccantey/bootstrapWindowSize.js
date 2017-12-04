59d0a181a3405a17dd78a0fbc696723d48eb540e
ccantey <cantey0701@gmail.com>
2017-12-04 14:45:44 -0600

    initial status

--- bootstrapWindowSizeHelper.js
+++ bootstrapWindowSizeHelper.js
@@ -4,6 +4,9 @@ $( document ).ready(function() {
     var w = $(document).innerWidth();
     return (w < 576) ? 'xs' : ((w < 768) ? 'sm' : ((w < 992) ? 'md' : ((w < 1200) ? 'lg' : 'xl')));
   }
+
+  console.log(getBootstrapBreakpoint());
+  
   $(window).on('resize', function(e) {
     clearTimeout(resizeTimer);
     resizeTimer = setTimeout(function() {
