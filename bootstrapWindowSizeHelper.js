f3ba7728e6b801ea618e58c2f4b1d6a733aba946
ccantey <cantey0701@gmail.com>
2017-12-04 14:39:26 -0600

    JavaScript Helper that logs Bootstrap media class

---
+++ bootstrapWindowSizeHelper.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+  var resizeTimer;
+  function getBootstrapBreakpoint(){
+    var w = $(document).innerWidth();
+    return (w < 576) ? 'xs' : ((w < 768) ? 'sm' : ((w < 992) ? 'md' : ((w < 1200) ? 'lg' : 'xl')));
+  }
+  $(window).on('resize', function(e) {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(function() {
+      console.log(getBootstrapBreakpoint());                    
+    }, 250);
+  });
+});
